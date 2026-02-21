ae5bf70e847c43cb925ddef22edc64bfe5bbea45
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-02-21 14:14:17 +0530

    changed animations

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -16,10 +16,10 @@ export default function ProductCard({
       {isFeatured && <Badge name="Featured" color="green"></Badge>}
       {isNew && <Badge name="New" color="orange"></Badge>}
       {isPopular && <Badge name="Popular" color="skyblue"></Badge>}
-      <li className="bg-[#4f39f6] p-4 rounded-full w-3 h-3 hidden group-hover:flex justify-center items-center absolute top-2 right-10 duration-250"><i className="fa-solid fa-heart text-white"></i></li>
+      <li className="bg-[#4f39f6] p-4 rounded-full w-3 h-3 hidden  group-hover:flex justify-center items-center absolute top-2 right-10 duration-250"><i className="fa-solid fa-heart text-white"></i></li>
       <li className="bg-[#4f39f6] p-4 rounded-full w-3 h-3 hidden group-hover:flex justify-center items-center absolute top-11 right-10 duration-250"><i className="fa-regular fa-eye text-white"></i></li>
       <li className="bg-[#4f39f6] p-4 rounded-full w-3 h-3 hidden group-hover:flex justify-center items-center absolute top-20 right-10 duration-250"><i className="fa-regular fa-bookmark text-white"></i></li>
-      <div className="border rounded shadow-md overflow-hidden w-9/10 h-8/10 mb-4 hover:shadow-xl">
+      <div className="border rounded shadow-md overflow-hidden w-9/10 h-8/10 mb-4 group-hover:shadow-2xl">
         <img
           src={`/images/${image}`}
           alt="product-pic"
